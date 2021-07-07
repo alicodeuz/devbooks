11d224c69f93b3a302e8742a229afc440132708d
Alisher778 <alisher.musurmonov89@gmail.com>
2021-07-07 10:22:43 +0500

    :sparkles: Added sign in page with hooks

--- src/containers/Auth/SignIn.js
+++ src/containers/Auth/SignIn.js
@@ -1,9 +1,83 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useHistory, Link } from 'react-router-dom';
+import classes from './SignIn.module.css';
 
-export default function SignIn() {
+export default function SignIn(props) {
+  const [state, setState] = useState({
+    email: '',
+    password: '',
+  });
+  const [visible, setVisible] = useState(null);
+  const [users, setUsers] = useState([]);
+
+  const handleInputChange = e => {
+    const { name, value } = e.target;
+    setState(prevState => ({ ...prevState, [name]: value }));
+  };
+
+  useEffect(() => {
+    console.log('I am effect');
+    if (visible) {
+      fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(data => setUsers(data))
+        .catch(err => {
+          console.error(err);
+        })
+    }
+  }, [visible, state.email]);
+
+  useEffect(() => {
+    console.log('I am effect');
+    const focusMethod = () => {
+      console.log("Focused");
+    }
+    window.addEventListener('focus', focusMethod);
+    return () => window.removeEventListener('focus', focusMethod);
+  }, [visible, state.email]);
+
   return (
     <div>
+      <h2 className={classes.active}>Hello</h2>
+      <div className="col-left">
 
+      </div>
+      <div className="col-right">
+        <h2>Sign In</h2>
+        <p>Do not you have an account? <Link to="/sign-up">Sign up</Link> </p>
+        <button onClick={() => setVisible(state => !state)}>
+          {visible ? 'Turn off' : 'Turn on'}
+        </button>
+
+        <form action="" className="form" autoComplete="off">
+          <input type="email" hidden name="email" />
+          <input type="password" hidden name="password" />
+
+          <div className="form__input-wrapper">
+            <input
+              type="text"
+              name="email"
+              value={state.email}
+              onChange={handleInputChange}
+              placeholder="Your email"
+              autoComplete="new-email"
+            />
+          </div>
+          <div className="form__input-wrapper">
+            <input
+              type="password"
+              name="password"
+              value={state.password}
+              onChange={handleInputChange}
+              placeholder="Your password"
+              autoComplete="new-password"
+            />
+          </div>
+          <div className="form__input-wrapper">
+            <button type="submit">Log in</button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
--- src/containers/Auth/SignUp.js
+++ src/containers/Auth/SignUp.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import classes from './SignUp.module.css';
 
 export default function SignUp() {
   return (
     <div>
-      <h2>Sign Up page</h2>
+      <h2 className={classes.active}>Sign Up</h2>
     </div>
   )
 }
